0b54d5fcb22d7c21fd1a1711bf7c0d4ec7efe991
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-07-26 15:30:25 +0100

    creating links

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,17 @@ class BookmarkManager < Sinatra::Base
     erb :'links/index'
   end
 
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    url = params[:url]
+    title = params[:title]
+    Link.create(url: url,title: title)
+    redirect '/links'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
