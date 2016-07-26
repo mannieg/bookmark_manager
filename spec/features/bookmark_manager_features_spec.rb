c5e9f841eeab6fa06cc73672140ee63f90241fb3
Manjinder Gill <manjinder.s.gill@googlemail.com>
2016-07-26 13:15:26 +0100

    Created a modular sinatra app

--- spec/features/bookmark_manager_features_spec.rb
+++ spec/features/bookmark_manager_features_spec.rb
@@ -4,6 +4,7 @@ feature '#viewing_links' do
     Link.create(title: 'google', url: 'www.google.com' )
 
     visit '/links'
+    expect(page.status_code).to eq 200
 
     within('ul#links') do
       expect(page).to have_content 'google'
