d80a770cf5713419892756a1c9cd8dfa6fa11cca
BartJudge <BartJudge@gmail.com>
2016-07-26 15:44:02 +0100

    exercise 11 addming a new link

---
+++ spec/features/creating_links_spec.rb
@@ -0,0 +1,17 @@
+require 'capybara/rspec'
+
+feature 'New links' do
+  scenario 'adding a new link to bookmark' do
+    visit '/links/new'
+    fill_in 'url', with: 'www.bbc.co.uk'
+    fill_in 'title', with: 'BBC'
+    click_button('Create_link')
+
+    expect(current_path).to eq '/links'
+
+    within 'ul#links' do
+      expect(page).to have_content "BBC"
+    end
+
+  end
+end
