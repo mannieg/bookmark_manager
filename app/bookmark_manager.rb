652f25e35db3fbbb95d747d7769244797a66be92
Luke Cartledge <luke@lukecartledge.com>
2016-07-26 19:06:04 +0100

    Ch16 adds filtering of tags

--- app/bookmark_manager.rb
+++ app/bookmark_manager.rb
@@ -23,9 +23,10 @@ class BookmarkManager < Sinatra::Base
     redirect '/links'
   end
 
- get '/tags/bubbles' do
-   @links = Link.all
-   erb(:'tags/filtered')
+ get '/tags/:name' do
+   tag = Tag.first(name: params[:name])
+   @links = tag ? tag.links : []
+   erb :'links/index'
  end
 
   # start the server if ruby file executed directly
--- spec/features/filtering_tags_spec.rb
+++ spec/features/filtering_tags_spec.rb
@@ -1,19 +1,20 @@
-feature 'filters tags' do
-  scenario 'user filters links by tag bubbles' do
-    visit '/links/new'
-    fill_in 'title', :with => 'Makers Academy'
-    fill_in 'url', :with => 'http://www.makersacademy.com'
+feature 'filtering by tags' do
+  before :each do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
+  end
 
-    fill_in 'tags', :with => 'coding'
-    click_button 'Add Link'
-    visit '/links/new'
-    fill_in 'title', :with => 'bubble Academy'
-    fill_in 'url', :with => 'http://www.bubblesacademy.com'
-    fill_in 'tags', :with => 'bubbles'
-    click_button 'Add Link'
-    visit 'tags/bubbles'
-    expect(page).to have_content "bubble Academy"
-    expect(page).not_to have_content "Makers Academy"
-  end 
+    scenario 'user filters links by tag bubbles' do
+      visit 'tags/bubbles'
 
+      expect(page.status_code).to eq 200
+      within 'ul#links' do
+        expect(page).to have_content 'This is Zombocom'
+        expect(page).to have_content 'Bubble Bobble'
+        expect(page).not_to have_content 'Makers Academy'
+        expect(page).not_to have_content 'Google'
+      end
+    end
 end
