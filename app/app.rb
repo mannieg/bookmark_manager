f03774c6f488c0ce54aa3e45bb220628d2ae0d88
JulieLev <jkgee1@yahoo.co.uk>
2016-07-26 14:53:43 +0100

    Ch 10 completed

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,11 @@
 require 'sinatra/base'
+require_relative 'models/link'
 
 class Bookmark_manager < Sinatra::Base
-  get '/' do
-    'Hello Bookmark_manager!'
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
   end
 
   # start the server if ruby file executed directly
--- spec/features/view_links_homepage_spec.rb
+++ spec/features/view_links_homepage_spec.rb
@@ -3,13 +3,13 @@ require 'capybara/rspec'
 feature 'See list of links' do
 
   scenario 'on homepage' do
-    Link.create(url: 'http://www.google.com', title: 'Google')
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/links'
 
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-      expect(page).to have_content "google"
+      expect(page).to have_content "makersacademy"
     end
   end
 end
