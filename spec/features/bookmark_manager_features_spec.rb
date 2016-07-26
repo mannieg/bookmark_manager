114e070f7ee709e00ea2130a321fdbd84a4bf8ca
Zeeshan Ishtiaq <mzishtiaq@yahoo.co.uk>
2016-07-26 11:47:25 +0100

    creates a link model

--- spec/features/bookmark_manager_features_spec.rb
+++ spec/features/bookmark_manager_features_spec.rb
@@ -2,8 +2,10 @@
 feature '#viewing_links' do
   scenario 'user views link on homepage' do
     Link.create(title: 'google', url: 'www.google.com' )
-    visit '/'
-    within('ul.links') do
+
+    visit '/links'
+
+    within('ul#links') do
       expect(page).to have_content 'google'
     end
   end
