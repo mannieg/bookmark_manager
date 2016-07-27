19fd28db0cb86eaa2f90871ac007e703817d00f0
JulieLev <jkgee1@yahoo.co.uk>
2016-07-27 17:54:35 +0100

    Ch 17 feature test for adding multiple tags passes

--- app/app.rb
+++ app/app.rb
@@ -15,10 +15,13 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url], title: params[:title])
-    tag  = Tag.first_or_create(name: params[:tags])  # 2. Create a tag for the link
-    link.tags << tag                       # 3. Adding the tag to the link's DataMapper collection.
-    link.save                              # 4. Saving the link.
+    link = Link.create(url: params[:url], title: params[:title])
+
+    params[:tags].split.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
+
+    link.save
     redirect '/links'
   end
 
